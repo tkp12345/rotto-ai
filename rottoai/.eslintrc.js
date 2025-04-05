0d9be394fc5b9a05a2817cd239b24fc3ef79604f
robertttttttto <roberto@bhaptics.com>
2025-04-06 02:40:31 +0900

    settings

---
+++ rottoai/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    extends: [
+        'next/core-web-vitals',
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        '@next/next/no-html-link-for-pages': 'off',
+    },
+};
+
